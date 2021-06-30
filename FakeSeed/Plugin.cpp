ad69ece5e922b78d2cc654aefd075ad819ef6e75
Qi You <1425321705@qq.com>
2021-06-30 23:08:43 +0800

    FakeSeed

--- FakeSeed/Plugin.cpp
+++ FakeSeed/Plugin.cpp
@@ -12,18 +12,38 @@
 #include "mc/Block.h"
 #include "mc/Player.h"
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <filesystem>
 using namespace std;
+
+#define CONF_PATH "./plugins/FakeSeed/seed.txt"
+int seed = -2138562307;
 
 void entry()
 {
+    try
+    {
+        if (!filesystem::exists(CONF_PATH))
+        {
+            filesystem::create_directories("./plugins/FakeSeed");
+            ofstream fout(CONF_PATH);
+            fout << seed << endl;
+        }
+        else
+        {
+            ifstream fin(CONF_PATH);
+            if (fin)
+                fin >> seed;
+        }
+    }
+    catch(...)  { }
     std::cout << "[FakeSeed] FackSeed Loaded." << endl;
+    std::cout << "[FakeSeed] Seed is set to " << seed << endl;
 }
 
-#define FAKE_SEED 12345
-THook(void*, "?write@StartGamePacket@@UEBAXAEAVBinaryStream@@@Z", void* a, void* b) {
-    if (FAKE_SEED) {
-        dAccess<int, 48>(a) = FAKE_SEED;
-    }
+THook(void*, "?write@StartGamePacket@@UEBAXAEAVBinaryStream@@@Z", void* a, void* b)
+{
+    dAccess<int, 48>(a) = seed;
     return original(a, b);
 }
