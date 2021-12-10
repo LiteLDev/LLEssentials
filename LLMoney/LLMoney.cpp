34371e5a8a2f3016684fcfe94eb5ebc52df241a5
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2021-12-10 20:57:09 +0800

    Update LLMoney.cpp

--- LLMoney/LLMoney.cpp
+++ LLMoney/LLMoney.cpp
@@ -14,48 +14,14 @@
 #include "Money.h"
 #include <filesystem>
 #include <JsonLoader.h>
+using namespace RegisterCommandHelper;
 
 double MoneyFee;
 bool initDB();
 
 extern money_t DEF_MONEY;
-enum MONEYOP :int {
-	query = 1,
-	hist = 2,
-};
-enum MONEYOP_pay :int {
-	pay = 1,
-	set = 2,
-	add = 3,
-	reduce = 4
-};
-enum MONEYOP_PURGE :int {
-	purge = 1
-};
-bool oncmd_money(CommandOrigin const& ori, CommandOutput& outp, MyEnum<MONEYOP> op, optional<string> dst) {
-	optional<xuid_t> dstid;
-	if (dst.set && ori.getPermissionsLevel() > 0) {
-		dstid = PlayerDB::getXuid(dst.val());
-	}
-	else {
-		dstid = PlayerDB::getXuid(ori.getName());
-	}
-	if (!dstid.Set()) {
-		outp.error(tr("money.no.target"));
-		return false;
-	}
-	switch (op)
-	{
-	case query:
-		outp.addMessage("Money: " + std::to_string(LLMoneyGet(dstid.val())));
-		break;
-	case hist:
-		outp.addMessage(LLMoneyGetHist(dstid.val()));
-		break;
-	}
-	return true;
-}
 
+/*
 bool oncmd_money_sel(CommandOrigin const& ori, CommandOutput& outp, MyEnum<MONEYOP> op, optional<CommandSelector<Player>> res) {
 	optional<xuid_t> dstid;
 	if (res.set && ori.getPermissionsLevel() > 0) {
@@ -81,85 +47,9 @@ bool oncmd_money_sel(CommandOrigin const& ori, CommandOutput& outp, MyEnum<MONEY
 	}
 	return true;
 }
+*/
 
-bool oncmd_money2(CommandOrigin const& ori, CommandOutput& outp, MyEnum<MONEYOP_pay> op, std::string dst, int val) {
-	optional<xuid_t> dstxuid, myuid;
-	dstxuid = PlayerDB::getXuid(dst);
-	if (!dstxuid.Set()) {
-		outp.error(tr("money.no.target"));
-		return false;
-	}
-	switch (op)
-	{
-	case pay:
-	{
-		if (val <= 0) {
-			outp.error(tr("money.invalid.arg"));
-			return false;
-		}
-		myuid = PlayerDB::getXuid(ori.getName());
-		if (!myuid.Set()) {
-			outp.error(tr("money.no.target"));
-			return false;
-		}
-		if (LLMoneyTrans(myuid.val(), dstxuid.val(), val, "money pay")) {
-			money_t fee = (money_t)(val * MoneyFee);
-			if (fee)
-				LLMoneyTrans(dstxuid.val(), 0, fee, "money pay fee");
-			outp.success("pay success");
-			return true;
-		}
-		else {
-			outp.error(tr("money.not.enough"));
-			return false;
-		}
-	}
-	break;
-	case set:
-		if (ori.getPermissionsLevel() < 1) {
-			outp.error(tr("money.no.perm"));
-			return false;
-		}
-		if (LLMoneySet(dstxuid.val(), val)) {
-			outp.success("set success");
-			return true;
-		}
-		else {
-			outp.error(tr("money.invalid.arg"));
-			return false;
-		}
-		break;
-	case add:
-		if (ori.getPermissionsLevel() < 1) {
-			outp.error(tr("money.no.perm"));
-			return false;
-		}
-		if (LLMoneyAdd(dstxuid.val(), val)) {
-			outp.success("add success");
-			return true;
-		}
-		else {
-			outp.error(tr("money.invalid.arg"));
-			return false;
-		}
-		break;
-	case reduce:
-		if (ori.getPermissionsLevel() < 1) {
-			outp.error(tr("money.no.perm"));
-			return false;
-		}
-		if (LLMoneyReduce(dstxuid.val(), val)) {
-			return true;
-		}
-		else {
-			outp.error(tr("money.invalid.arg"));
-			return false;
-		}
-		break;
-	}
-	return true;
-}
-
+/*
 bool oncmd_money2_sel(CommandOrigin const& ori, CommandOutput& outp, MyEnum<MONEYOP_pay> op, CommandSelector<Player> res, int val) {
 	if (res.results(ori).empty()) {
 		outp.error(tr("money.no.target"));
@@ -243,19 +133,154 @@ bool oncmd_money2_sel(CommandOrigin const& ori, CommandOutput& outp, MyEnum<MONE
 	}
 	return true;
 }
+*/
 
-bool oncmd_money3_p(CommandOrigin const& ori, CommandOutput& outp, MyEnum<MONEYOP_PURGE> op, optional<int> difftime) {
-	if (ori.getPermissionsLevel() < 1) {
-		outp.error(tr("money.no.perm"));
-		return false;
+
+class MoneyCommand : public Command {
+	enum MoneyOP :int {
+		query = 1,
+		hist = 2,
+		pay = 3,
+		set = 4,
+		add = 5,
+		reduce = 6,
+		purge = 7
+	} op;
+	string dst;
+	bool dst_isSet;
+	bool difftime_isSet;
+	int moneynum;
+	int difftime;
+public:
+	void execute(CommandOrigin const& ori, CommandOutput& outp) const {
+		optional<xuid_t> dstid;
+		optional<xuid_t> dstxuid, myuid;
+		switch (op)
+		{
+		case query:
+		case hist:
+			if (dst_isSet && ori.getPermissionsLevel() > 0) {
+				dstid = PlayerDB::getXuid(dst);
+			}
+			else {
+				dstid = PlayerDB::getXuid(ori.getName());
+			}
+			if (!dstid.Set()) {
+				outp.error(tr("money.no.target"));
+				return;
+			}
+			break;
+		case pay:
+		case set:
+		case add:
+		case reduce:
+			dstxuid = PlayerDB::getXuid(dst);
+			if (!dstxuid.Set()) {
+				outp.error(tr("money.no.target"));
+				return;
+			}
+			break;
+		case purge:
+			if (ori.getPermissionsLevel() < 1) {
+				outp.error(tr("money.no.perm"));
+				return;
+			}
+			break;
+		}
+		switch (op)
+		{
+		case query:
+			outp.addMessage("Money: " + std::to_string(LLMoneyGet(dstid.val())));
+			break;
+		case hist:
+			outp.addMessage(LLMoneyGetHist(dstid.val()));
+			break;
+		case pay:
+		{
+			if (moneynum <= 0) {
+				outp.error(tr("money.invalid.arg"));
+			}
+			myuid = PlayerDB::getXuid(ori.getName());
+			if (!myuid.Set()) {
+				outp.error(tr("money.no.target"));
+			}
+			if (LLMoneyTrans(myuid.val(), dstxuid.val(), moneynum, "money pay")) {
+				money_t fee = (money_t)(moneynum * MoneyFee);
+				if (fee)
+					LLMoneyTrans(dstxuid.val(), 0, fee, "money pay fee");
+				outp.success("pay success");
+			}
+			else {
+				outp.error(tr("money.not.enough"));
+			}
+		}
+		break;
+		case set:
+			if (ori.getPermissionsLevel() < 1) {
+				outp.error(tr("money.no.perm"));
+			}
+			if (LLMoneySet(dstxuid.val(), moneynum)) {
+				outp.success("set success");
+			}
+			else {
+				outp.error(tr("money.invalid.arg"));
+			}
+			break;
+		case add:
+			if (ori.getPermissionsLevel() < 1) {
+				outp.error(tr("money.no.perm"));
+			}
+			if (LLMoneyAdd(dstxuid.val(), moneynum)) {
+				outp.success("add success");
+			}
+			else {
+				outp.error(tr("money.invalid.arg"));
+			}
+			break;
+		case reduce:
+			if (ori.getPermissionsLevel() < 1) {
+				outp.error(tr("money.no.perm"));
+			}
+			if (LLMoneyReduce(dstxuid.val(), moneynum)) {
+			}
+			else {
+				outp.error(tr("money.invalid.arg"));
+			}
+			break;
+		case purge:
+			if (difftime_isSet)
+				LLMoneyClearHist(difftime);
+			else
+				LLMoneyClearHist(0);
+		}
+		return;
 	}
-	if (difftime.Set())
-		LLMoneyClearHist(difftime.val());
-	else
-		LLMoneyClearHist(0);
-	return true;
-}
+	static void setup(CommandRegistry* registry) {
 
+		//registerCommand
+		registry->registerCommand(
+			"money", "money system", CommandPermissionLevel::Any, { (CommandFlagValue)0 }, { (CommandFlagValue)0x80 });
+		//addEnum
+		registry->addEnum<MoneyOP>("MoneyOP1", { { "query",MoneyOP::query},{ "hist",MoneyOP::hist}});
+		registry->addEnum<MoneyOP>("MoneyOP2", { { "add",MoneyOP::add},{ "pay",MoneyOP::pay },{"reduce",MoneyOP::reduce},{"set",MoneyOP::set}});
+		registry->addEnum<MoneyOP>("MoneyOP3", { { "purge",MoneyOP::purge} });
+
+		//registerOverload
+		registry->registerOverload<MoneyCommand>(
+			"money",
+			makeMandatory<CommandParameterDataType::ENUM>(&MoneyCommand::op, "optional", "MoneyOP1"),
+			makeOptional(&MoneyCommand::dst, "target", &MoneyCommand::dst_isSet));
+		registry->registerOverload<MoneyCommand>(
+			"money",
+			makeMandatory<CommandParameterDataType::ENUM>(&MoneyCommand::op, "optional", "MoneyOP2"),
+			makeMandatory(&MoneyCommand::dst, "target"),
+			makeMandatory(&MoneyCommand::moneynum, "num"));
+		registry->registerOverload<MoneyCommand>(
+			"money",
+			makeMandatory<CommandParameterDataType::ENUM>(&MoneyCommand::op, "optional", "MoneyOP3"),
+			makeOptional(&MoneyCommand::difftime, "time", &MoneyCommand::difftime_isSet));
+	}
+};
 
 void entry() {
 	std::filesystem::create_directory("plugins\\LLMoney");
@@ -265,17 +290,9 @@ void entry() {
 	}
 	Logger::setTitle("Money");
 	Translation::load("plugins\\LLMoney\\langpack\\money.json");
-	Event::addEventListener([](RegCmdEvent ev) {
-		MakeCommand("money", "money", 0);
-		MakeCommand("money_s", "money(CommandSelector)", 1);
-		CEnum<MONEYOP> _1("type", { "query","hist" });
-		CEnum<MONEYOP_pay> _2("type2", { "pay","set","add","reduce" });
-		CEnum<MONEYOP_PURGE> _3("mpurge", { "purge" });
-		CmdOverload(money, oncmd_money, "op", "target");
-		CmdOverload(money, oncmd_money2, "op", "target1", "money");
-		CmdOverload(money, oncmd_money3_p, "purge", "difftime");
-		CmdOverload(money_s, oncmd_money_sel, "op", "target");
-		CmdOverload(money_s, oncmd_money2_sel, "op", "target1", "money");
+	Event::RegCmdEvent::subscribe([](const Event::RegCmdEvent& ev) {
+		MoneyCommand::setup(ev.mCommandRegistry);
+		return true;
 		});
 	try {
 		ConfigJReader jr("plugins\\LLMoney\\money.json");
