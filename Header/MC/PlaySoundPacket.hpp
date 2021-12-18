8c123fcddb977999dee031ab9aea1b95d29c447a
ShrBox <shrbox@outlook.com>
2021-12-18 19:18:54 +0800

    Sync Header && Lib

--- Header/MC/PlaySoundPacket.hpp
+++ Header/MC/PlaySoundPacket.hpp
@@ -13,7 +13,7 @@ class PlaySoundPacket : public Packet {
 
 #define AFTER_EXTRA
 // Add Member There
-
+    char filler[104];
 #undef AFTER_EXTRA
 
 #ifndef DISABLE_CONSTRUCTOR_PREVENTION_PLAYSOUNDPACKET
@@ -24,11 +24,11 @@ public:
 
 public:
     /*0*/ virtual ~PlaySoundPacket();
-    /*1*/ virtual int /*enum enum MinecraftPacketIds*/ getId() const;
+    /*1*/ virtual enum MinecraftPacketIds getId() const;
     /*2*/ virtual std::string getName() const;
     /*3*/ virtual void write(class BinaryStream&) const;
     /*4*/ virtual bool disallowBatching() const;
-    /*5*/ virtual int /*enum enum StreamReadResult*/ _read(class ReadOnlyBinaryStream&);
+    /*5*/ virtual enum StreamReadResult _read(class ReadOnlyBinaryStream&);
     /*
     inline  ~PlaySoundPacket(){
          (PlaySoundPacket::*rv)();
